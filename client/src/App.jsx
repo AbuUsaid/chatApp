793233acb22b70fd33b63407f476d4b62a08aa48
AbuUsaid <usaidabu321@gmail.com>
2024-07-24 19:06:40 +0530

    add input and start chat button and logic in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
+import io from 'socket.io-client';
 
+const socket = io('http://localhost:5000');
 const App = () => {
-  return <div>App</div>;
+  const [username, setUserName] = useState('');
+  const [chatActive, setChatActive] = useState(false);
+
+  return (
+    <>
+      <div className="w-screen h-screen bg-gray-100">
+        {chatActive ? (
+          <div>chat is here</div>
+        ) : (
+          <div className="w-screen h-screen flex justify-center items-center gap-2">
+            <input
+              type="text"
+              name=""
+              id=""
+              value={username}
+              onChange={(e) => setUserName(e.target.value)}
+              className="text-center px-3 py-2 outline-none border-2 rounded-md"
+            />
+            <button
+              type="submit"
+              className="bg-green-500 text-white px-3 py-2 rounded-md font-bold"
+              onClick={() => {
+                !username == '' && setChatActive(true);
+              }}
+            >
+              Start chat
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default App;
