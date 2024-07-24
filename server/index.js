b88564dfaa916f164f637a8eef3c313790692ad8
AbuUsaid <usaidabu321@gmail.com>
2024-07-24 18:43:07 +0530

    Set up basic server and integrate Tailwind CSS in React client

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected`);
+  });
+});
+
+server.listen(5000, () => {
+  console.log(`🟢 Server running on ${`http://localhost:5173/`}`);
+});
